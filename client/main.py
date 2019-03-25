382cbffb37ee7d8a6761e83603afab4d42b8623d
Lucas <lucaslaukien@gmail.com>
2019-03-24 21:51:08 -0400

    Seperated gui from game, added on key up, centered game, scaled game, cleaned up .kv file

--- client/main.py
+++ client/main.py
@@ -8,43 +8,15 @@ from kivy.graphics import Color, Rectangle
 
 from world import *
 
-class PongPaddle(Widget):
-	score = NumericProperty(0)
-
-	def bounce_ball(self, ball):
-		if self.collide_widget(ball):
-			vx, vy = ball.velocity
-			offset = (ball.center_y - self.center_y) / (self.height / 2)
-			bounced = Vector(-1 * vx, vy)
-			vel = bounced * 1.1
-			ball.velocity = vel.x, vel.y + offset
-
-
-class PongBall(Widget):
-	velocity_x = NumericProperty(0)
-	velocity_y = NumericProperty(0)
-	velocity = ReferenceListProperty(velocity_x, velocity_y)
-
-	def move(self):
-		self.pos = Vector(*self.velocity) + self.pos
-
-
 class PongGame(Widget):
-	ball = ObjectProperty(None)
-	player1 = ObjectProperty(None)
-	player2 = ObjectProperty(None)
-
 	def __init__(self, world, **kwargs):
 		super(PongGame, self).__init__(**kwargs)
 		self.world = world
-		self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
-		self._keyboard.bind(on_key_down=self._on_keyboard_down)
+		self.keyboard = Window.request_keyboard(self.keyboard_closed, self)
+		self.keyboard.bind(on_key_down=self.on_keyboard_down)
+		self.keyboard.bind(on_key_up=self.on_keyboard_up)
 		self.time_passed = 0.0
 		self.ticks = 0
-
-	def serve_ball(self, vel=(4, 0)):
-		self.ball.center = self.center
-		self.ball.velocity = vel
 	
 	def tick(self, dt):
 		self.world.tick(dt)
@@ -56,17 +28,21 @@ class PongGame(Widget):
 	def update(self, dt):
 		self.time_passed += dt
 
+		self.ticks = 0
 		while self.time_passed >= 1.0 / 60.0:
 			self.tick(1.0 / 60)
 			self.time_passed -= 1.0 / 60.0
+			self.ticks += 1
+			if self.ticks >= 6:
+				self.time_passed = 0.0
 
 		self.render(dt)
 
-	def _keyboard_closed(self):
-		self._keyboard.unbind(on_key_down=self._on_keyboard_down)
-		self._keyboard = None
+	def keyboard_closed(self):
+		self.keyboard.unbind(on_key_down=self.on_keyboard_down)
+		self.keyboard = None
 
-	def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+	def on_keyboard_down(self, keyboard, keycode, text, modifiers):
 		if keycode[1] == 'w':
 			self.world.player1.vy = 10
 		elif keycode[1] == 's':
@@ -77,11 +53,16 @@ class PongGame(Widget):
 			self.world.player2.vy = -10
 		return True
 
-	def on_touch_move(self, touch):
-		if touch.x < self.width / 3:
-			self.player1.center_y = touch.y
-		if touch.x > self.width - self.width / 3:
-			self.player2.center_y = touch.y
+	def on_keyboard_up(self, keyboard, keycode):
+		if keycode[1] == 'w':
+			self.world.player1.vy = 0.0
+		elif keycode[1] == 's':
+			self.world.player1.vy = 0.0
+		elif keycode[1] == 'up':
+			self.world.player2.vy = 0.0
+		elif keycode[1] == 'down':
+			self.world.player2.vy = 0.0
+		return True
 
 
 class PongApp(App):
@@ -91,7 +72,6 @@ class PongApp(App):
 
 	def build(self):
 		game = PongGame(self.world)
-		game.serve_ball()
 		Clock.schedule_interval(game.update, 1.0 / 240.0)
 		return game
 
--- client/world.py
+++ client/world.py
@@ -1,7 +1,9 @@
 from kivy.graphics import Color, Rectangle, Ellipse
+from kivy.graphics.context_instructions import Translate, Scale
 
 class Ball:
-	def __init__(self):
+	def __init__(self, world):
+		self.world = world
 		self.x = 0.0
 		self.y = 0.0
 		self.vx = 0.0
@@ -15,24 +17,25 @@ class Ball:
 		if self.x < 0.0:
 			self.x = 0.0
 			self.vx = -self.vx
-		elif self.x > 19.0:
-			self.x = 19.0
+		elif self.x > self.world.root - self.radius * 2.0:
+			self.x = self.world.root - self.radius * 2.0
 			self.vx = -self.vx
 
 		if self.y < 0.0:
 			self.y = 0.0
 			self.vy = -self.vy
-		elif self.y > 19.0:
-			self.y = 19.0
+		elif self.y > self.world.root - self.radius * 2.0:
+			self.y = self.world.root - self.radius * 2.0
 			self.vy = -self.vy
 	
-	def render(self, canvas, ratio, dt):
+	def render(self, canvas, dt):
 		with canvas:
 			Color(0.2, 0.5, 0.7, 1.0)
-			Ellipse(pos=(self.x * ratio, self.y * ratio), size=(self.radius * 2.0 * ratio, self.radius * 2.0 * ratio))
+			Ellipse(pos=(self.x, self.y), size=(self.radius * 2.0, self.radius * 2.0))
 
 class Player:
-	def __init__(self):
+	def __init__(self, world):
+		self.world = world
 		self.x = 0.0
 		self.y = 0.0
 		self.vy = 0.0
@@ -45,22 +48,22 @@ class Player:
 		if self.y < 0.0:
 			self.y = 0.0
 			self.vy = 0.0
-		elif self.y + 3.0 > 20.0:
-			self.y = 17.0
+		elif self.y + self.height > self.world.root:
+			self.y = self.world.root - self.height
 			self.vy = 0.0
 		
-	def render(self, canvas, ratio, dt):
+	def render(self, canvas, dt):
 		with canvas:
 			Color(0.5, 0.5, 0.5, 1.0)
-			Rectangle(pos=(self.x * ratio, self.y * ratio), size=(self.width * ratio, self.height * ratio))
+			Rectangle(pos=(self.x, self.y), size=(self.width, self.height))
 
 class World:
 	def __init__(self):
 		self.root = 20.0
-		self.ball = Ball()
-		self.player1 = Player()
-		self.player2 = Player()
-		self.player2.x = 19.2
+		self.ball = Ball(self)
+		self.player1 = Player(self)
+		self.player2 = Player(self)
+		self.player2.x = self.root - self.player2.width
 		self.ball.vx = 5.0
 		self.ball.vy = 7.0
 
@@ -71,10 +74,20 @@ class World:
 
 	def render(self, canvas, size, dt):
 		ratio = 1.0
+		width_is_bigger = True
 		if size[0] > size[1]:
 			ratio = size[1] / self.root
 		else:
+			width_is_bigger = False
 			ratio = size[0] / self.root
-		self.player1.render(canvas, ratio, dt)
-		self.player2.render(canvas, ratio, dt)
-		self.ball.render(canvas, ratio, dt)
+		with canvas:
+			if width_is_bigger:
+				Translate(size[0] / 2.0 - self.root / 2.0 * ratio, 0.0)
+			else:
+				Translate(0.0, size[1] / 2.0 - self.root / 2.0 * ratio)
+			Scale(ratio, ratio, 1.0)
+			Color(0.6, 0.2, 0.2, 1.0)
+			Rectangle(pos=(0, 0), size=(self.root, self.root))
+		self.player1.render(canvas, dt)
+		self.player2.render(canvas, dt)
+		self.ball.render(canvas, dt)
